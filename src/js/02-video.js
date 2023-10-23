ec149264bbedfb94fe33b68407749087dadb4e19
Anastazzia <malashevychanna@gmail.com>
2023-10-23 20:53:04 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const lodash = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player(iframe);
+
+vimeoPlayer.on('timeupdate', function (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
+
+vimeoPlayer.on('timeupdate', throttle(function (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000));
