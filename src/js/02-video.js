f71976a2a8e7d8a8173fce5afece3eea4dfce54f
Anastazzia <malashevychanna@gmail.com>
2023-10-29 16:33:03 +0200

    gg

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,18 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-
-const lodash = require('lodash.throttle');
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const vimeoPlayer = new Player(iframe);
 
-vimeoPlayer.on('timeupdate', function (event) {
-    const currentTime = event.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime.toString());
-});
 
-const savedTime = localStorage.getItem('videoplayer-current-time');
+const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME);
 if (savedTime) {
     vimeoPlayer.setCurrentTime(parseFloat(savedTime));
 }
 
 vimeoPlayer.on('timeupdate', throttle(function (event) {
     const currentTime = event.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+    localStorage.setItem(VIDEO_CURRENT_TIME, currentTime);
 }, 1000));
